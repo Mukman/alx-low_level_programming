c2d39803a729690192287b8ab32aff06bae5a060
Mukman <endris64muktar@gmail.com>
2023-10-05 09:23:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - this is the main put function
@@ -9,4 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
-
